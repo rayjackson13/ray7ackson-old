237e386c12f8c96d22d169aae0acea68a0c33174
Ray Jackson <rayjackson132@gmail.com>
2018-09-29 10:51:52 +0700

    fff

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
 var autoprefixer = require('gulp-autoprefixer');
 var htmlbeautify = require('gulp-html-beautify');
 var cleanCSS = require('gulp-clean-css');
@@ -34,7 +35,9 @@ gulp.task('serve', ['sass'], function() {
         server: ['./', './src']
     });
 
-    gulp.watch(["./src/sass/*.scss", "./src/sass/**/*.scss", "./src/sass/sections/*.scss", "node_modules/bootstrap/scss/bootstrap.scss"], ['sass']);
+    gulp.watch(["./src/sass/*.scss", "./src/sass/**/*.scss", "./src/sass/sections/*.scss", "node_modules/bootstrap/scss/bootstrap.scss"], function (event, cb) {
+        setTimeout(function(){ gulp.start('sass'); }, 500)
+    });
     gulp.watch(['src/js/*.js'], ['js']);
     gulp.watch([ 'src/babel/*.js' ], [ 'babel' ]).on('change', browserSync.reload);
     gulp.watch(["./src/*.html"], ['htmlbeautify']).on('change', browserSync.reload);
@@ -43,6 +46,7 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(["./src/sass/*.scss", "./src/sass/**/*.scss", "./src/sass/sections/*.scss", "node_modules/bootstrap/scss/bootstrap.scss"])
+        .pipe(sassGlob())
         .pipe(sass())
          .pipe(autoprefixer({
              browsers: ['last 2 versions'],
